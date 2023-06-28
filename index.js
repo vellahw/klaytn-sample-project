c8d3a34b022e36383fdf897222842f88dbacf588
Vella <gksdnjs2@gmail.com>
2023-06-28 11:58:04 +0900

    routing setting

--- index.js
+++ index.js
@@ -13,4 +13,16 @@ app.set('view engine', 'ejs')
 // post 방식으로 데이터가 들어오는 경우 json 형태로 데이터를 변환
 app.use(express.urlencoded({extended:false}))
 
+// localhost:3000 [get] 요청이 들어왔을 때
+app.get('/', (req, res)=>
+    res.redirect('/user')
+)
+
+// routing 사용
+const sql = require('./routes/sql.js')
+app.use('/user', sql)
+
+const contract = require('./routes/contract.js')
+app.use('/contract', contract)
+
 const server = app.listen(port, ()=>console.log(port, "Server Start!!"))
